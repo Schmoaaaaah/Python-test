a766679945a4120daa1cd10e0f345bc1299b53d1
Noah Kempers <noah.kempers@hbbk-office.de>
2020-09-14 09:58:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+def main():
+  print("Hello World")
+  Name = input("Wie heißt du? \n")
+  print("Hallo " + Name + ".")
+  if(Name == "Max"):
+    i=0
+    for i in range(1,30):
+      print(Name + " ist blöd")
+  elif(Name == "Noah"):
+    print(Name + " ist voll nett")
+
+def Variablen():
+  hallo = "Hallo"
+  Name = "Noah"
+  print(2*(hallo+Name))
+  foo ="foo"
+  print(foo)
+  print(type(foo))
+  foo = 1
+  print(foo)
+  print(type(foo))
+
+def Listen():
+  import random
+  lul = ["Hallo","Hi","Guten Tag"]
+  print(random.uniform(0,len(lul)))
+  print("begruesung " + lul[0])
+
+def Schmeicheln():
+  import random
+  import os
+  adjektiv = ["beste","schönste","schlauste","schnellste"]
+  nomen = ["Mensch","Freund","Lehrer"]
+
+  def menue():
+    os.system('clear') ##😔
+    auswahl = int(input("1 = adjektiv hinzufügen; 2 = nomen hinzufügen; 3 = random ausgabe"))
+    if auswahl == 1:
+      addadj()
+    elif auswahl == 2:
+      addnom()
+    elif auswahl == 3:
+      ausgabe()
+    else:
+      print("fehlerhafte eingabe! Tschüsss")
+
+
+  def ausgabe():
+    print("Du bist der " + random.choice(adjektiv) + " " + random.choice(nomen) + "!")
+    menue()
+
+  def addadj():
+    adjektiv.append(input("Welches Adjektiv soll hinzugefügt werden?"))
+    menue()
+  
+  def addnom():
+    nomen.append(input("Welches Nomen soll hinzugefügt werden?"))
+    menue()
+  
+  menue()
+
+Listen()
